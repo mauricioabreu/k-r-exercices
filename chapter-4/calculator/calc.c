a838f49ca1bb2e112edf78788c2a8e4267c1f80a
Maurício Antunes <mauricio.abreua@gmail.com>
2020-01-04 19:06:10 -0300

    Refactor and start to handle variables

--- chapter-4/calculator/calc.c
+++ chapter-4/calculator/calc.c
@@ -5,6 +5,7 @@
 
 #define MAXOP 100 // max size of operand or operator
 #define NUMBER '0' // signal that a number was found
+#define MAXVARS 26 // max number of variables (alphabet)
 
 int getop(char []);
 void push(double);
@@ -14,7 +15,8 @@ void print(void);
 void duplicate(void);
 void clear(void);
 int count(void);
-int is_word(char []);
+void set(unsigned char, double);
+double get(unsigned char);
 
 // commands
 const char PRINT = 'p';
@@ -25,38 +27,36 @@ const char CLEAR = 'c';
 const int SIN = 256;
 const int EXP = 257;
 const int POW = 258;
+const int SET = 259;
+const int GET = 260;
 
 char specials[3];
+
+double vars[MAXVARS];
 
 /* reverse polish calculator */
 int main()
 {
     int type;
-    int is_command = 0;
     double op2;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF) {
         switch (type) {
             case NUMBER:
-                is_command = 0;
                 push(atof(s));
                 break;
             case '+':
-                is_command = 0;
                 push(pop() + pop());
                 break;
             case '*':
-                is_command = 0;
                 push(pop() * pop());
                 break;
             case '-':
-                is_command = 0;
                 op2 = pop();
                 push(pop() - op2);
                 break;
             case '/':
-                is_command = 0;
                 op2 = pop();
                 if (op2 != 0.0)
                     push(pop() / op2);
@@ -64,7 +64,6 @@ int main()
                     printf("error: zero divisor\n");
                 break;
             case '%':
-                is_command = 0;
                 op2 = pop();
                 push(fmod(pop(), op2));
                 break;
@@ -79,20 +78,20 @@ int main()
                 push(pow(pop(), op2));
                 break;
             case PRINT:
-                is_command = 1;
                 print();
                 break;
             case DUPLICATE:
-                is_command = 1;
                 duplicate();
                 break;
             case CLEAR:
-                is_command = 1;
                 clear();
                 printf("stack cleared\n");
                 break;
+            case SET:
+                set(s[0], get_last());
+                break;
             case '\n':
-                if (!is_command && count() > 0)
+                if (count() > 0)
                     printf("\t%.8g\n", get_last());
                 break;
             default:
@@ -155,72 +154,50 @@ void ungetch(int);
 int getop(char s[])
 {
     int i, c;
-
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
-        ;
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.' && c != '-') {
-        specials[0] = c;
-        if (specials[0] == '\n') {
-            return c;
-        }
-
-        specials[1] = getch();
-        if (specials[1] == '\n') {
-            ungetch(specials[1]);
-            return specials[0];
-        }
-
-        specials[2] = getch();
-        if (specials[2] == '\n') {
-            ungetch(specials[1]);
-            ungetch(specials[2]);
-            return specials[0];
-        }
-
-        // a word may be a special expression like sin
-        if (is_word(specials))
-            if (specials[0] == 's' && specials[1] == 'i' && specials[2] == 'n')
-                return SIN;
-            if (specials[0] == 'e' && specials[1] == 'x' && specials[2] == 'p')
-                return EXP;
-            if (specials[0] == 'p' && specials[1] == 'o' && specials[2] == 'w')
-                return POW;
-
-        ungetch(specials[1]);
-        ungetch(specials[2]);
-        return c; // not a number
-    }
-
     i = 0;
 
-    if (c == ' ') {
-        if (isdigit(c=getch()) || c == '.')
-            s[++i] = c;
-        else 
-        {
-            if (c != EOF)
-                ungetch(c);
-            return '-';
-        }
-    }
-    
-    if (isdigit(c)) // collect integer part
-        while (isdigit(s[++i] = c = getch()))
-            ;
-    if (c == '.') // collect fraction part
-        while (isdigit(s[++i] = c = getch()))
-            ;
-    s[i] = '\0';
-    if (c != EOF)
+    while ((s[0] = c = getch()) == ' ' || c == '\t') // skip blanks and tabs
+        ;
+    s[1] = '\0';
+
+    if (c == '\n' || c == EOF) // return return carriage or end of line right away
+        return c;
+
+    if (isalpha(c)) {
+        while (isalpha(s[++i] = c = getch()));
         ungetch(c);
-    return NUMBER;
+        s[i] = '\0';
+
+        if (i > 1) {    // special function like SIN, POW, etc
+            if (strcmp(s, "sin") == 0)
+                return SIN;
+            if (strcmp(s, "exp") == 0)
+                return EXP;
+            if (strcmp(s, "pow") == 0)
+                return POW;
+        } else {    // handle vars
+            getch();
+            return s[0];
+        }
+    } else if (isdigit(c)) {
+        while (isdigit(s[++i] = c = getch()))
+            ;
+        if (c == '.') // collect fraction part
+            while (isdigit(s[++i] = c = getch()))
+                ;
+
+        ungetch(c);
+        s[i] = '\0';
+        return NUMBER;
+    }
+
+    return c;
 }
 
 /* print: print last element of stack */
 void print(void)
 {
-    printf("print top element %g\n", get_last());
+    printf("print top element: %g\n", get_last());
 }
 
 /* duplicate: duplicate the last element of stack */
@@ -255,12 +232,14 @@ void clear(void)
     sp = 0;
 }
 
-/* is_word: if all chars are alpha */
-int is_word(char letters[])
+/* set: create a var with the given n value */
+void set(unsigned char var, double n)
 {
-    for (int i = 0; letters[i] != '\0'; i++) {
-        if (!isalpha(letters[i]))
-            return 0;
-    }
-    return 1;
+    vars[var] = n;
+}
+
+/* get: return the variable value */
+double get(unsigned char var)
+{
+    return vars[var];
 }
